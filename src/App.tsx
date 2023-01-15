622f3325bfc3310b8d759ae993c52e35ef376e6d
Johan Wulf <johanwulf98@gmail.com>
2023-01-15 16:39:42 +0100

    more styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
   return (
     <>
       <div className="flex flex-col content-center w-full max-w-md">
-        <div className="navbar bg-zinc-900 mt-4">
+        <div className="navbar bg-zinc-900">
           <div className="navbar-middle">
             <button
               className="btn btn-square btn-link btn-ghost"
