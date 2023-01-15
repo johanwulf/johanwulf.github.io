a7f74f7c149b5c0ff46a73f4aae76a6fa882fa22
Johan Wulf <johanwulf98@gmail.com>
2023-01-15 18:00:30 +0100

    feat: new age function to calculate age automatically

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,13 +1,21 @@
 export const Introduction = () => {
+  const getAge = () => {
+    var ageDifMs = Date.now() - new Date("1998-08-26").getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+
+  console.log(getAge());
   return (
     <div
       className={`flex flex-col gap-1 transition-all duration-500 ease-in-out opacity-100 text-white mb-4`}
     >
       <p className="text-xl font-semibold">Hello!</p>
       <div className="indent-3">
-        My name is Johan Wulf and I am a 25-year-old Junior Software Engineer
-        currently working at IKEA. I have a passion for technology and am always
-        striving to learn new things, both in frontend and backend development.
+        My name is Johan Wulf and I am a {getAge()}-year-old Junior Software
+        Engineer currently working at IKEA. I have a passion for technology and
+        am always striving to learn new things, both in frontend and backend
+        development.
       </div>
     </div>
   );
