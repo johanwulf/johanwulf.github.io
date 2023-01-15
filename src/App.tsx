5218fbde62e54a5c90b11c30c4cbef8a483b70ed
Johan Wulf <johanwulf98@gmail.com>
2023-01-15 16:27:57 +0100

    fix mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-col w-screen">
         <div className="navbar bg-zinc-900 mt-4">
           <div className="navbar-middle">
             <button
@@ -77,108 +77,110 @@ function App() {
             </button>
           </div>
         </div>
-        <div
-          className={`flex flex-col gap-1 transition-all duration-500 ease-in-out w-80 opacity-100 text-white mb-4`}
-        >
-          <p className="text-l font-semibold">Hello!</p>
-          <div className="indent-3">
-            My name is Johan Wulf and I am a 25-year-old Junior Software
-            Engineer currently working at IKEA. I have a passion for technology
-            and am always striving to learn new things, both in frontend and
-            backend development.
-          </div>
-          <div className="indent-3">
-            I am particularly interested in web development, and have experience
-            with languages such as JavaScript, HTML, CSS, and Python. I also
-            love working with different frameworks like React, Angular and
-            Node.js. When I am not working, I enjoy spending my free time
-            training my beloved puppy.
-          </div>
-        </div>
-        <div>
-          {view === View.HOME && (
-            <div
-              className={`flex flex-col gap-1 transition-all duration-500 ease-in-out w-96 ${
-                view === View.HOME && fade === false
-                  ? "opacity-100"
-                  : "opacity-0"
-              }`}
-            >
-              <h1 className="text-xl text-white font-bold">Biography</h1>
-              <div className="flex gap-4">
-                <h1 className="text-l text-white font-bold">1998</h1>
-                <p className="text-white">Born in Växjö, Sweden</p>
-              </div>
-              <div className="flex gap-4">
-                <h1 className="text-l text-white font-bold">2022</h1>
-                <p className="text-white">
-                  Graduated from Lunds Tekniska Högskola, Bachelor of Science in
-                  Engineering, Computer Science and Engineering
-                </p>
-              </div>
-              <div className="flex gap-4">
-                <h1 className="text-l text-white font-bold">2022</h1>
-                <p className="text-white">
-                  Began working for IKEA as a Junior Software Engineer
-                </p>
-              </div>
+        <div className="mr-2 ml-2">
+          <div
+            className={`flex flex-col gap-1 transition-all duration-500 ease-in-out w-80 opacity-100 text-white mb-4`}
+          >
+            <p className="text-l font-semibold">Hello!</p>
+            <div className="indent-3">
+              My name is Johan Wulf and I am a 25-year-old Junior Software
+              Engineer currently working at IKEA. I have a passion for
+              technology and am always striving to learn new things, both in
+              frontend and backend development.
             </div>
-          )}
-          {view === View.ABOUT && (
-            <div
-              className={`flex flex-col gap-1 transition-all duration-500 ease-in-out w-96 ${
-                view === View.ABOUT && fade === false
-                  ? "opacity-100"
-                  : "opacity-0"
-              }`}
-            >
-              <h1 className="text-xl text-white font-bold">About</h1>
-              <div className="flex gap-4">
-                <h1 className="text-l text-white font-bold">Lorem</h1>
-                <p className="text-white">Lorem Ipsum bla bla bla</p>
-              </div>
-              <div className="flex gap-4">
-                <h1 className="text-l text-white font-bold">Lorem</h1>
-                <p className="text-white">Lorem Ipsum bla bla bla</p>
-              </div>
-              <div className="flex gap-4">
-                <h1 className="text-l text-white font-bold">Lorem</h1>
-                <p className="text-white">Lorem ipsum bla bla bla</p>
-              </div>
+            <div className="indent-3">
+              I am particularly interested in web development, and have
+              experience with languages such as JavaScript, HTML, CSS, and
+              Python. I also love working with different frameworks like React,
+              Angular and Node.js. When I am not working, I enjoy spending my
+              free time training my beloved puppy.
             </div>
-          )}
+          </div>
+          <div>
+            {view === View.HOME && (
+              <div
+                className={`flex flex-col gap-1 transition-all duration-500 ease-in-out w-auto ${
+                  view === View.HOME && fade === false
+                    ? "opacity-100"
+                    : "opacity-0"
+                }`}
+              >
+                <h1 className="text-xl text-white font-bold">Biography</h1>
+                <div className="flex gap-4">
+                  <h1 className="text-l text-white font-bold">1998</h1>
+                  <p className="text-white">Born in Växjö, Sweden</p>
+                </div>
+                <div className="flex gap-4">
+                  <h1 className="text-l text-white font-bold">2022</h1>
+                  <p className="text-white">
+                    Graduated from Lunds Tekniska Högskola, Bachelor of Science
+                    in Engineering, Computer Science and Engineering
+                  </p>
+                </div>
+                <div className="flex gap-4">
+                  <h1 className="text-l text-white font-bold">2022</h1>
+                  <p className="text-white">
+                    Began working for IKEA as a Junior Software Engineer
+                  </p>
+                </div>
+              </div>
+            )}
+            {view === View.ABOUT && (
+              <div
+                className={`flex flex-col gap-1 transition-all duration-500 ease-in-out w-auto ${
+                  view === View.ABOUT && fade === false
+                    ? "opacity-100"
+                    : "opacity-0"
+                }`}
+              >
+                <h1 className="text-xl text-white font-bold">About</h1>
+                <div className="flex gap-4">
+                  <h1 className="text-l text-white font-bold">Lorem</h1>
+                  <p className="text-white">Lorem Ipsum bla bla bla</p>
+                </div>
+                <div className="flex gap-4">
+                  <h1 className="text-l text-white font-bold">Lorem</h1>
+                  <p className="text-white">Lorem Ipsum bla bla bla</p>
+                </div>
+                <div className="flex gap-4">
+                  <h1 className="text-l text-white font-bold">Lorem</h1>
+                  <p className="text-white">Lorem ipsum bla bla bla</p>
+                </div>
+              </div>
+            )}
 
-          {view === View.CONTACT && (
-            <div
-              className={`flex flex-col gap-2 transition-all duration-500 ease-in-out w-96 ${
-                view === View.CONTACT && fade === false
-                  ? "opacity-100"
-                  : "opacity-0"
-              }`}
-            >
-              <h1 className="text-xl text-white font-bold">Find me</h1>
-              <a
-                className="flex gap-4"
-                href="https://www.linkedin.com/in/johanwulf/"
-                target="_blank"
+            {view === View.CONTACT && (
+              <div
+                className={`flex flex-col gap-2 transition-all duration-500 ease-in-out w-auto ${
+                  view === View.CONTACT && fade === false
+                    ? "opacity-100"
+                    : "opacity-0"
+                }`}
               >
-                <img src="./linkedin.svg" className="invert" />
-                <p className="text-white">LinkedIn</p>
-              </a>
-              <a
-                className="flex gap-4"
-                href="https://www.github.com/johanwulf"
-                target="_blank"
-              >
-                <img src="./github.svg" className="invert" />
-                <p className="text-white">GitHub</p>
-              </a>
-              <a className="flex gap-4" href="mailto:johan@wulf.gg">
-                <img src="./mail.svg" className="invert" />
-                <p className="text-white">johan@wulf.gg</p>
-              </a>
-            </div>
-          )}
+                <h1 className="text-xl text-white font-bold">Find me</h1>
+                <a
+                  className="flex gap-4"
+                  href="https://www.linkedin.com/in/johanwulf/"
+                  target="_blank"
+                >
+                  <img src="./linkedin.svg" className="invert" />
+                  <p className="text-white">LinkedIn</p>
+                </a>
+                <a
+                  className="flex gap-4"
+                  href="https://www.github.com/johanwulf"
+                  target="_blank"
+                >
+                  <img src="./github.svg" className="invert" />
+                  <p className="text-white">GitHub</p>
+                </a>
+                <a className="flex gap-4" href="mailto:johan@wulf.gg">
+                  <img src="./mail.svg" className="invert" />
+                  <p className="text-white">johan@wulf.gg</p>
+                </a>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
