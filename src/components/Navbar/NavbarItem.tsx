056400b3eb5ceeb78895161824a27405c63f52a3
johanwulf <johanwulf98@gmail.com>
2023-05-11 06:36:56 +0200

    style: fix formatting

--- src/components/Navbar/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem.tsx
@@ -1,26 +1,25 @@
 import { View } from "../../types/view";
 
 type NavbarProps = {
-  title: string;
-  view: View;
-  currentView: View;
-  onClick: (view: View) => void;
+    title: string;
+    view: View;
+    currentView: View;
+    onClick: (view: View) => void;
 };
 
 export const NavbarItem = ({
-  title,
-  view,
-  currentView,
-  onClick,
+    title,
+    view,
+    currentView,
+    onClick,
 }: NavbarProps) => {
-  return (
-    <button
-      className={`btn btn-ghost btn-link lowercase ${
-        view === currentView ? "text-orange-500" : "text-orange-300"
-      } no-underline hover:underline font-bold`}
-      onClick={() => onClick(view)}
-    >
-      {title}
-    </button>
-  );
+    return (
+        <button
+            className={`btn btn-ghost btn-link lowercase ${view === currentView ? "text-orange-500" : "text-orange-300"
+                } no-underline hover:underline font-bold`}
+            onClick={() => onClick(view)}
+        >
+            {title}
+        </button>
+    );
 };
