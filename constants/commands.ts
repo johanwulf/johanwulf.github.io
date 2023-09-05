a7944a6ddc6bc2d094b4925287da6aeb07cec475
johanwulf <johanwulf98@gmail.com>
2023-09-05 14:47:11 +0200

    fix: prettierrc config and code cleanup

--- constants/commands.ts
+++ constants/commands.ts
@@ -1,29 +1,29 @@
 export enum FileType {
-  FILE,
-  FOLDER,
-  EXECUTEABLE,
+    FILE,
+    FOLDER,
+    EXECUTEABLE,
 }
 
 export const fileSystem = [
-  {
-    name: "about.txt",
-    type: FileType.FILE,
-    path: "~",
-    content: "hi im johan",
-  },
-  {
-    name: "readme.txt",
-    type: FileType.FILE,
-    path: "~",
-    content: "actually please don't read me",
-  },
-  {
-    name: "welcome.sh",
-    type: FileType.EXECUTEABLE,
-    path: "~",
-    content:
-      'echo "Welcome to my website. If you are comfortable with the terminal and tmux, please have a look around! If not, write help and press enter."',
-  },
-  { name: "fun-things", type: FileType.FOLDER, path: "~" },
-  { name: "another.txt", type: FileType.FILE, path: "~/fun-things/" },
+    {
+        name: "about.txt",
+        type: FileType.FILE,
+        path: "~",
+        content: "hi im johan",
+    },
+    {
+        name: "readme.txt",
+        type: FileType.FILE,
+        path: "~",
+        content: "actually please don't read me",
+    },
+    {
+        name: "welcome.sh",
+        type: FileType.EXECUTEABLE,
+        path: "~",
+        content:
+            'echo "Welcome to my website. If you are comfortable with the terminal and tmux, please have a look around! If not, write help and press enter."',
+    },
+    { name: "fun-things", type: FileType.FOLDER, path: "~" },
+    { name: "another.txt", type: FileType.FILE, path: "~/fun-things", content: "Another one" },
 ];
--- src/App.tsx
+++ src/App.tsx
@@ -2,129 +2,119 @@ import { useEffect, useRef, useState } from "react";
 import { FileType, fileSystem } from "../constants/commands";
 import "./App.scss";
 
-const validCommands = ["ls", "cd", "clear", "cat"];
-
 function App() {
-  const [path, setPath] = useState("~");
-  const [command, setCommand] = useState("");
-  const [log, setLog] = useState<
-    { command: string; output: string | null; path: string }[]
-  >([
-    {
-      command: "./welcome.sh",
-      output:
-        "Welcome to my website. If you are comfortable with the terminal and tmux, please have a look around! If not, write help and press enter.",
-      path: "~",
-    },
-  ]);
-  const inputRef = useRef<HTMLInputElement>(null);
+    const [path, setPath] = useState("~");
+    const [command, setCommand] = useState("");
+    const [log, setLog] = useState<{ command: string; output: string | null; path: string }[]>([
+        {
+            command: "./welcome.sh",
+            output: "Welcome to my website. If you are comfortable with the terminal and tmux, please have a look around! If not, write help and press enter.",
+            path: "~",
+        },
+    ]);
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    const handleClick = () => {
-      if (inputRef.current) {
-        inputRef.current.focus();
-      }
-    };
+    useEffect(() => {
+        const handleClick = () => {
+            if (inputRef.current) {
+                inputRef.current.focus();
+            }
+        };
 
-    document.addEventListener("click", handleClick);
+        document.addEventListener("click", handleClick);
 
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-  }, []);
+        return () => {
+            document.removeEventListener("click", handleClick);
+        };
+    }, []);
 
-  const handleKeyDown = (e: any) => {
-    if (e.key === "Enter") {
-      const newLogEntry = { command, output: "", path: path };
-      const c = command.split(" ")[0];
-      const a = command.split(" ")[1];
+    const handleKeyDown = (e: any) => {
+        if (e.key === "Enter") {
+            const newLogEntry = { command, output: "", path: path };
+            const cmd = command.split(" ")[0].trim();
+            const arg = command.split(" ")[1];
 
-      setLog((log) => [...log, newLogEntry]);
-      if (validCommands.includes(c)) {
-        if (command === "ls") {
-          newLogEntry.output = fileSystem
-            .filter((e) => e.path === path)
-            .map((e) => e.name)
-            .join(" ");
-        } else if (command === "clear") {
-          setLog([]);
-        } else if (command === "./welcome.sh") {
-          newLogEntry.output =
-            "Welcome to my website. If you are comfortable with the terminal and tmux, please have a look around! If not, write help and press enter.";
-        } else if (c === "cd") {
-          const f = fileSystem
-            .filter((e) => e.path === path && e.type == FileType.FOLDER)
-            .map((e) => e.name);
+            const entry = fileSystem.filter((e) => e.path === path);
+            const files = entry.filter((e) => e.type === FileType.EXECUTEABLE || e.type === FileType.FILE);
+            const folders = entry.filter((e) => e.type === FileType.FOLDER);
 
-          if (a === "..") {
-            setPath("~");
-            newLogEntry.output = " ";
-          } else if (a === "" || a === " ") {
-            setPath("~");
-            newLogEntry.output = " ";
-          } else if (f.length === 0) {
-            newLogEntry.output = "No such folder";
-          } else {
-            setPath(f[0]);
-            newLogEntry.output = " ";
-          }
-        } else if (c === "cat") {
-          const files = fileSystem.filter(
-            (e) =>
-              e.path === path &&
-              (e.type === FileType.FILE || e.type === FileType.EXECUTEABLE) &&
-              e.name === a
-          );
-          if (files.length < 1) {
-            newLogEntry.output = `zsh: file not found: ${a}`;
-          } else {
-            newLogEntry.output = files[0].content!;
-          }
+            setLog((log) => [...log, newLogEntry]);
+            switch (cmd) {
+                case "ls":
+                    newLogEntry.output = entry.map((e) => e.name).join(" ");
+                    break;
+                case "clear":
+                    setLog([]);
+                    break;
+                case "cd":
+                    if (!arg) {
+                        setPath("~");
+                    } else if (arg === "..") {
+                        setPath("~");
+                        newLogEntry.output = " ";
+                    } else if (folders.length === 0) {
+                        newLogEntry.output = "No such folder";
+                    } else {
+                        setPath("~/" + folders[0].name);
+                        newLogEntry.output = " ";
+                    }
+                    break;
+                case "cat":
+                    const f = files.filter((e) => e.name === arg);
+                    if (f.length < 1) {
+                        newLogEntry.output = `zsh: file not found: ${arg}`;
+                    } else {
+                        newLogEntry.output = f[0].content!;
+                    }
+                    break;
+                case "":
+                    newLogEntry.output = "";
+                    break;
+                default:
+                    newLogEntry.output = `zsh: command not found: ${command.split(" ")[0]}`;
+                    break;
+            }
+
+            setCommand("");
         }
-      } else {
-        newLogEntry.output = `zsh: command not found: ${command.split(" ")[0]}`;
-      }
-
-      setCommand("");
-    }
-  };
-  return (
-    <div className="main-content">
-      <div className="title-bar">
-        <div className="title-bar-buttons">
-          <div className="title-bar-buttons-red"></div>
-          <div className="title-bar-buttons-yellow"></div>
-          <div className="title-bar-buttons-green"></div>
-        </div>
-        johan.wulf - tmux
-      </div>
-      <div className="terminal">
-        {log.map((entry, index) => (
-          <div key={index} className="log-entry">
-            <div className="tilde">{entry.path}</div>
-            <div className="arrow">
-              ❯ <div className="output">{entry.command}</div>
+    };
+    return (
+        <div className="main-content">
+            <div className="title-bar">
+                <div className="title-bar-buttons">
+                    <div className="title-bar-buttons-red"></div>
+                    <div className="title-bar-buttons-yellow"></div>
+                    <div className="title-bar-buttons-green"></div>
+                </div>
+                johan.wulf - tmux
             </div>
-            {entry.output && <div className="output">{entry.output}</div>}
-          </div>
-        ))}
-        <div className="prompt">
-          <div className="tilde">{path}</div>{" "}
-          <div className="arrow">
-            ❯{" "}
-            <input
-              ref={inputRef}
-              value={command}
-              onChange={(e) => setCommand(e.target.value)}
-              className="text"
-              onKeyDown={handleKeyDown}
-            />
-          </div>
+            <div className="terminal">
+                {log.map((entry, index) => (
+                    <div key={index} className="log-entry">
+                        <div className="tilde">{entry.path}</div>
+                        <div className="arrow">
+                            ❯ <div className="output">{entry.command}</div>
+                        </div>
+                        {entry.output && <div className="output">{entry.output}</div>}
+                    </div>
+                ))}
+                <div className="prompt">
+                    <div className="tilde">{path}</div>{" "}
+                    <div className="arrow">
+                        ❯{" "}
+                        <input
+                            ref={inputRef}
+                            value={command}
+                            onChange={(e) => setCommand(e.target.value)}
+                            className="text"
+                            onKeyDown={handleKeyDown}
+                        />
+                    </div>
+                </div>
+                <div className="tmux">1:home* 2:projects 3:about</div>
+            </div>
         </div>
-        <div className="tmux">1:home* 2:projects 3:about</div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
