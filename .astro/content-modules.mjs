1ba323b6f4936df4717addd7d3dcb481b21ea11c
Johan Wulf <johan.wulf@ingka.ikea.com>
2025-06-08 15:57:04 +0200

    blog: add blog post about shadecn

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
+["src/content/blog/building-shadecn.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbuilding-shadecn.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/migrating-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmigrating-to-astro.mdx&astroContentModuleFlag=true")]]);
 		
