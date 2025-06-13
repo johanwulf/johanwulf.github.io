eae61390117c733e16956c6b1573a97362c13fc8
johanwulf <johanwulf98@gmail.com>
2025-06-13 14:58:56 +0200

    blog: add ci cd cost spike blog post

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,6 @@
 
 export default new Map([
 ["src/content/blog/building-shadecn.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fbuilding-shadecn.mdx&astroContentModuleFlag=true")],
-["src/content/blog/migrating-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmigrating-to-astro.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/migrating-to-astro.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fmigrating-to-astro.mdx&astroContentModuleFlag=true")],
+["src/content/blog/ci-cd-cost-spike.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fci-cd-cost-spike.mdx&astroContentModuleFlag=true")]]);
 		
